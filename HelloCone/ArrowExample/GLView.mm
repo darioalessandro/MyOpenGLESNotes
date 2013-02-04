b8afe10200a2d0bbee96b3012f2256c5e1196e3c
Dario Lencina <darioalessandro@me.com>
2013-02-03 20:48:50 -0500

    CONE EXAMPLE

---
+++ HelloCone/ArrowExample/GLView.mm
@@ -0,0 +1,77 @@
+//
+//  GLView.m
+//  ArrowExample
+//
+//  Created by Dario Lencina on 2/3/13.
+//  Copyright (c) 2013 Dario Lencina. All rights reserved.
+//
+
+#import "GLView.h"
+
+@implementation GLView
+
+- (id) initWithFrame: (CGRect) frame
+{
+    if (self = [super initWithFrame:frame]) {
+        CAEAGLLayer* eaglLayer = (CAEAGLLayer*) super.layer;
+        eaglLayer.opaque = YES;
+        EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
+        m_context = [[EAGLContext alloc] initWithAPI:api];
+        if (!m_context || TRUE) {
+            api = kEAGLRenderingAPIOpenGLES1;
+            m_context = [[EAGLContext alloc] initWithAPI:api]; }
+        if (!m_context || ![EAGLContext setCurrentContext:m_context]) { 
+            return nil;
+        }
+        if (api == kEAGLRenderingAPIOpenGLES1) {
+            m_renderingEngine = CreateRenderer1();
+        } else {
+            m_renderingEngine = CreateRenderer2();
+        }
+        [m_context renderbufferStorage:GL_RENDERBUFFER fromDrawable: eaglLayer];
+        m_renderingEngine->Initialize(CGRectGetWidth(frame), CGRectGetHeight(frame));
+        [self drawView: nil];
+        m_timestamp = CACurrentMediaTime();
+        CADisplayLink* displayLink= [CADisplayLink displayLinkWithTarget:self
+                                                  selector:@selector(drawView:)];
+        [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+        
+        [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(didRotate:)
+                                                     name:UIDeviceOrientationDidChangeNotification
+                                                   object:nil];
+    }
+    return self;
+}
+
++ (Class) layerClass
+{
+    return [CAEAGLLayer class];
+}
+
+- (void) drawView: (CADisplayLink*) displayLink
+{
+    if (displayLink != nil) {
+        float elapsedSeconds = displayLink.timestamp - m_timestamp;
+        m_timestamp = displayLink.timestamp;
+        m_renderingEngine->UpdateAnimation(elapsedSeconds);
+    }
+    m_renderingEngine->Render();
+    [m_context presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+- (void) didRotate: (NSNotification*) notification
+{
+    UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
+    m_renderingEngine->OnRotate((DeviceOrientation) orientation);
+    [self drawView: nil];
+}
+
+- (void) dealloc
+{
+    if ([EAGLContext currentContext] == m_context)
+        [EAGLContext setCurrentContext:nil];
+}
+
+@end
---
+++ HelloCone/ArrowExample/IRenderingEngine.hpp
@@ -0,0 +1,34 @@
+//
+//  IRenderingEngine.h
+//  ArrowExample
+//
+//  Created by Dario Lencina on 2/3/13.
+//  Copyright (c) 2013 Dario Lencina. All rights reserved.
+//
+
+// Physical orientation of a handheld device, equivalent to UIDeviceOrientation.
+
+enum DeviceOrientation {
+    DeviceOrientationUnknown,
+    DeviceOrientationPortrait,
+    DeviceOrientationPortraitUpsideDown,
+    DeviceOrientationLandscapeLeft,
+    DeviceOrientationLandscapeRight,
+    DeviceOrientationFaceUp,
+    DeviceOrientationFaceDown
+};
+// Creates an instance of the renderer and sets up various OpenGL state.
+
+struct IRenderingEngine* CreateRenderer1();
+struct IRenderingEngine* CreateRenderer2();
+
+// Interface to the OpenGL ES renderer; consumed by GLView.
+
+struct IRenderingEngine {
+    virtual void Initialize(int width, int height) = 0;
+    virtual void Render() const = 0;
+    virtual void UpdateAnimation(float timeStep) = 0;
+    virtual void OnRotate(DeviceOrientation newOrientation) = 0;
+    virtual ~IRenderingEngine() {}
+};
+
